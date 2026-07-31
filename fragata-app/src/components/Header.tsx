b71a62cc8ac5b6166960525463d3d74a48f29818
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-07-31 08:40:31 -0300

    Update Header.tsx

--- fragata-app/src/components/Header.tsx
+++ fragata-app/src/components/Header.tsx
@@ -185,24 +185,12 @@ export function Header() {
           Contato
         </button>
 
-        {/* Mobile Menu Icon 
-        
+        {/* Mobile Menu Icon*/}
+
         <button ref={mobileMenuRef} className="md:hidden text-primary opacity-0">
-          <svg
-            className="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
+          Contato
         </button>
-        */}
+
       </div>
     </nav>
   )
